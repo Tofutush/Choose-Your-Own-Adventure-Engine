587d166fa242217cec258fef7cf49b510a47936f
Tofutush <xiexuanqisirocco@163.com>
2022-10-10 10:37:00 +0800

    folders

--- src/cyoa.js
+++ src/cyoa.js
@@ -5,7 +5,7 @@ var page = { // stuff on document,body
 	display: document.getElementById('display'),
 	options: document.getElementById('options')
 };
-const deadOptions = loadXML('deadOptions.xml'); // do we load it like that? or do we load them as separate options?
+const deadOptions = loadXML('src/deadOptions.xml'); // do we load it like that? or do we load them as separate options?
 
 function elt(type,props,...children){let dom=document.createElement(type);if(props)Object.assign(dom,props);for(let child of children){if(typeof child!="string")dom.appendChild(child);else dom.appendChild(document.createTextNode(child));}return(dom);}
 
@@ -112,7 +112,7 @@ function Display(dom, parent) {
 }
 
 Display.prototype.parseImg = function(img) { // do we even need these two one-line functions
-	return elt('img', {src: 'comic/'+img.firstChild.nodeValue});
+	return elt('img', {src: 'assets/'+img.firstChild.nodeValue});
 };
 
 Display.prototype.parseText = function(txt) {
@@ -189,3 +189,67 @@ Options.prototype.show = function() {
 		page.options.appendChild(this.elts[z].button);
 	};
 };
+
+// code from https://www.30secondsofcode.org/articles/s/js-data-structures-tree
+
+class TreeNode {
+	constructor(key, value = key, parent = null) {
+		this.key = key;
+		this.value = value;
+		this.parent = parent;
+		this.children = [];
+	}
+	get isLeaf() {
+		return this.children.length === 0;
+	}
+	get hasChildren() {
+		return !this.isLeaf;
+	}
+}
+
+class Tree {
+	constructor(key, value = key) {
+		this.root = new TreeNode(key, value);
+	}
+	*preOrderTraversal(node = this.root) {
+		yield node;
+		if (node.children.length) {
+			for (let child of node.children) {
+				yield* this.preOrderTraversal(child);
+			}
+		}
+	}
+	*postOrderTraversal(node = this.root) {
+		if (node.children.length) {
+			for (let child of node.children) {
+				yield* this.postOrderTraversal(child);
+			}
+		}
+		yield node;
+	}
+	insert(parentNodeKey, key, value = key) {
+		for (let node of this.preOrderTraversal()) {
+			if (node.key === parentNodeKey) {
+				node.children.push(new TreeNode(key, value, node));
+				return true;
+			}
+		}
+		return false;
+	}
+	remove(key) {
+		for (let node of this.preOrderTraversal()) {
+			const filtered = node.children.filter(c => c.key !== key);
+			if (filtered.length !== node.children.length) {
+				node.children = filtered;
+				return true;
+			}
+		}
+		return false;
+	}
+	find(key) {
+		for (let node of this.preOrderTraversal()) {
+			if (node.key === key) return node;
+		}
+		return undefined;
+	}
+}
